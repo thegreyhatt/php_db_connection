cda934c71155233f2a5d69ce5b0781c01fb3c678
ervin_beñez <ervinbenez@gmail.com>
2018-08-28 19:26:06 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP DB Connection</title>
+</head>
+<body>
+
+<?php require 'links.html';  ?>
+
+<!-- <a href="users.php">View Users</a>
+<a href="add-new.php">Add New User</a> -->
+</body>
+</html>
